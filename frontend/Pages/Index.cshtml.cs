98b768e9903dc87172c78ad28999d9f3228c073d
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2021-02-12 19:01:16 +0100

    Add Frist Sample

--- frontend/Pages/Index.cshtml.cs
+++ frontend/Pages/Index.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -17,9 +14,11 @@ namespace frontend.Pages
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        public WeatherForecast[] Forecasts { get; set; }
 
+        public async Task OnGet([FromServices] WeatherClient client)
+        {
+            Forecasts = await client.GetWeatherAsync();
         }
     }
 }
